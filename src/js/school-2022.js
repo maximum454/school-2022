27a4b1d0d57c3287a644a835f745815adac1314d
Maxim Karachyov <karachyov.m@gmail.com>
2022-07-12 12:25:52 +0300

    desktop

--- src/js/school-2022.js
+++ src/js/school-2022.js
@@ -1,1 +1,11 @@
 @@include('./partials/swiper-bundle.min.js')
+
+const swiperSchool2022 = new Swiper('.swiper-school-2022', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
